3b70b622c18401f4ed8371572e3600a7cfac6e89
Goutham Pai <goutham.r.pai@gmail.com>
2022-05-04 01:31:37 -0500

    DI

---
+++ CartTaxCalculator/Program.cs
@@ -0,0 +1,29 @@
+﻿using CartTaxCalculator.Services;
+using CartTaxCalculator.Services.Rules;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((_, services) =>
+        services
+        .AddTransient<IRule, DomesticSalesTaxRule>()
+        .AddTransient<IRule, ImportTaxRule>()
+        .AddTransient<ITaxCalculationService, TaxCalculationService>()
+        .AddTransient<IInvoiceService, InvoiceService>()
+        .AddTransient<IProcessCartDataService, ProcessCartDataService>()
+    )
+    .Build();
+
+Run(host.Services);
+
+await host.RunAsync();
+
+static void Run(IServiceProvider services)
+{
+    using IServiceScope serviceScope = services.CreateScope();
+    IServiceProvider provider = serviceScope.ServiceProvider;
+
+    var processCartDataService = provider.GetRequiredService<IProcessCartDataService>();
+    processCartDataService.GetCartsAndPrintInvoices();
+    Console.WriteLine();
+}
